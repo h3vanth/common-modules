00e255204b254e0f985aedf05e50188a7dddd6ff
h3vanth <rhevanthraj@gmail.com>
2024-10-06 17:40:49 +0530

    Remove log from event handler

--- common/src/main/java/io/formulate/common/event/EventHandler.java
+++ common/src/main/java/io/formulate/common/event/EventHandler.java
@@ -2,17 +2,14 @@ package io.formulate.common.event;
 
 import com.google.common.eventbus.EventBus;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@Slf4j
 public class EventHandler {
     private final EventBus eventBus;
 
     public void emitEvent(Event event) {
-        log.info("[{}] Emitting event {}", Thread.currentThread().getName(), event);
         eventBus.post(event);
     }
 
